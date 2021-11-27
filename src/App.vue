fa6c450c8b14992ef62ef37e49b0fe54aae87bd0
AldenBurgess <arburgess98@gmail.com>
2021-11-27 16:53:58 -0600

    is this thing on

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <navigation/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,11 +8,22 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/includes/Navigation.vue'
+
+import Vue from 'vue';
+import App from './App.vue'
+import router from './router'; //import router.js into main.js
+new Vue({
+  render: h => h(App),
+  //store,(where does this come in?)
+  router //added router instance
+}).$mount('#app')
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navigation
   }
 }
 </script>
